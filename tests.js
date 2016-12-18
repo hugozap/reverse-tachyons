94d0e750943c349b419046a1b5ca5c56e65647a8
Hugo Zapata <hugozap@gmail.com>
2016-12-17 23:06:43 -0500

    added responsive test

--- tests.js
+++ tests.js
@@ -2,19 +2,41 @@ var test = require('tape')
 var r = require('./index')
 
 test('basic', function (t) {
-    t.plan(1)
-    r.getCSSRules('w-100', (err, rules) => {
-        t.equals(rules.defaultRules[0], 'width: 100%')
-    })
+  t.plan(1)
+  r.getCSSRules('w-100', (err, rules) => {
+    t.equals(rules.defaultRules[0], 'width: 100%')
+  })
 })
 
 test('js 1', function (t) {
-   t.plan(1)
-    r.getJSObjectRules('w-100 bg-black', (err, obj) => { 
-        const expectedDefault = {
-            width: '100%',
-            backgroundColor:'#000'
-        }
-        t.equals(JSON.stringify(obj.defaultRules), JSON.stringify(expectedDefault))
-    })
+  t.plan(1)
+  r.getJSObjectRules('w-100 bg-black', (err, obj) => {
+    const expectedDefault = {
+      width: '100%',
+      backgroundColor: '#000'
+    }
+    t.equals(JSON.stringify(obj.defaultRules), JSON.stringify(expectedDefault))
+  })
+})
+
+test('responsive 1', function (t) {
+  t.plan(4)
+  r.getJSObjectRules('w-100 w-50-ns w-10-m w-20-l', (err, ob) => {
+    const expecteDefault = {
+      width: '100%'
+    }
+    const expectedNotSmall = {
+      width: '50%'
+    }
+    const expectedMedium = {
+      width: '10%'
+    }
+    const expectedLarge = {
+      width: '20%'
+    }
+    t.deepEqual(ob.defaultRules, expecteDefault)
+    t.deepEqual(ob.nsRules, expectedNotSmall)
+    t.deepEqual(ob.mRules, expectedMedium)
+    t.deepEqual(ob.lRules, expectedLarge)
+  })
 })
